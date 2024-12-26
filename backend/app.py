8b3155e5ef19e3deae443c62e220ba8856691880
meghavinodkumar <meghasden@gmail.com>
2024-12-26 11:27:06 +0530

    API modifications

--- backend/app.py
+++ backend/app.py
@@ -1,106 +1,148 @@
 import json
 import random
 from flask import Flask, jsonify, request
-#Creating a new flask application 
-app= Flask(__name__)
-#Array of cart items - sample products in the Uniblox store
-products=[ { 'id': 1, 'name': 'Torch', 'price':'100', 'url':'http://' },
+
+# Create a new flask application 
+app = Flask(__name__)
+
+#################### Data storage ####################
+# Since we are not using a persistant storage here, the following in-memory variables are used for the temporary storage
+
+# List of products
+products = [ { 'id': 1, 'name': 'Torch', 'price':'100', 'url':'http://' },
            { 'id': 2, 'name': 'Frying Pan', 'price':'400' , 'url':'http://'},
-            { 'id': 3, 'name': 'Basketball','price':'800', 'url':'http://' }
+           { 'id': 3, 'name': 'Basketball','price':'800', 'url':'http://' }
         ]
-#ASSUMPTIONS : since the authentication module is missing a user_iD of 123 and "n" orders for discount are assumed 
-user_id=123
-n=3
-discount_percentage=10
-#In memory data storage
-orders=[]
-cart=[]
-coupons=[]
+
+# List of orders
+orders = []
+
+# Current shopping cart items
+cart = {'items' : [], 'total_price' :0, 'discount_eligibility': False, 'coupon_applied' : False, }
+
+# Coupons for current user
+coupons = []
+
+################## Assumptions #################
+# Since the authentication module is not impemented a user_iD of 123 is assumed
+user_id = 123
+
+# Number of orders that make a user eligible for a discount 
+n_orders = 3
+
+# Discount percentage that is applicable for the 'n'th order
+discount_percentage = 10
+
+############# Products APIs ###############
 @app.route('/products', methods=['GET'])
-def get_products():
-    """List the products"""
+def get_products_list():
+    """ List all the products """
+    global products
     return jsonify(products)
+
+############### Cart APIs ################
+
+def update_total_and_discount_eligibility(cart):
+    if (len(cart['items'])):
+        cart['total_price'] = get_total_price()
+        cart['discount_eligibility'] = is_eligible_for_discount(n_orders)
 
 @app.route('/items',methods=['POST'])
 def add_item_to_cart():
-    """Add items to cart"""
+    """ Add an item to cart """
+    global cart
     item=json.loads(request.data)
-    #Error handling if there is no product found
-    # TODO: add check here for null condition***************************
-    if not item:
-        return jsonify({'error': 'Product not found'}), 404
-     #storing in temporary in memory 
-    cart.append(item)
+    cart['items'].append(item)
+    update_total_and_discount_eligibility(cart)
     return jsonify(item)
 
 @app.route('/items', methods=['GET'])
 def get_items_in_cart():
-    """Get all items in the cart"""
+    """ Get the list of all items in the cart along with the total and eligibility for discount """
+    global cart
+    # if (len(cart['items'])):
+    #     cart['total_price'] = get_total_price()
+    #     cart['discount_eligibility'] = is_eligible_for_discount(n_orders)
     return jsonify(cart)
 
 @app.route('/items/<id>', methods=['DELETE'])
-def delete_items_in_cart(id):
-    """Get all items in the cart"""
-    print(id)
-    for item in cart:
+def delete_item_from_cart(id):
+    """ Remove an item from the cart """
+    global cart
+    id = int(id)
+    for index, item in enumerate(cart['items']):
         if item["id"]==id:
-            cart.pop(id)
+            cart['items'].pop(index) 
+            break
     return jsonify(item)
 
+############### Coupons APIs ############
 @app.route('/coupons', methods=['POST'])
 def generate_coupon():
-    """Generate coupon code"""
-    coupon_code = random.randint(1000, 9999)
-    coupons.append({"user_id":user_id, "code": coupon_code})
-    return jsonify({'user_id':user_id,'code': coupon_code})
-
-@app.route('/coupons', methods=['GET'])
-def get_coupon():
-    """Get Couponcode"""
-    return jsonify({'coupons':coupons})
-
+    """ Generate a coupon code and store it for later use """
+    global coupons
+    # For now, use a randomly generated 6 digit number as the coupon code
+    coupon_code = random.randint(100000, 999999)
+    coupon = {"user_id":user_id, "code": coupon_code}
+    coupons.append(coupon)
+    return jsonify(coupon)
 
 def is_valid_code(code):
-    #check if the code is valid or not 
+    # check if the code is valid 
+    global coupons
     for coupon in coupons:
         if ((coupon["user_id"] == user_id) and (coupon["code"] == code)):
             return True
     return False
 
+def get_total_price():
+    global cart
+    totalPrice = 0
+    for item in cart['items']:
+        totalPrice += item["price"]
+    return totalPrice
 
 @app.route('/apply_coupon/<code>', methods=['POST'])
 def apply_coupon(code):
-    """Apply the coupon code"""
-    #check if the code is a valid coupon code 
-    totalPrice=0
-    for item in cart:
-        totalPrice+=item["price"]
+    """ Apply the coupon code for the items in the cart"""
+    # Check if the given couplecode is a valid one first
+    totalPrice = get_total_price()
     if is_valid_code(int(code)):
         totalPrice = totalPrice - (totalPrice*discount_percentage/100)
-    return jsonify({'totalPrice':totalPrice})
+        cart['coupon_applied'] = True
+        cart['total_price'] = totalPrice
+    return jsonify({'total_price':totalPrice})
 
-#check if previous n-1 orders did not apply coupon if so return a boolean flag indicating that this order is eligible for discount 
-def is_eligible_for_discount():
+# check whether the last count-1 orders did not apply any coupons. 
+# If so return a boolean flag indicating that this order is eligible for discount 
+def is_eligible_for_discount(count):
+    global orders
+    # Count how many orders that the user has placed just before this order without discount
     num_orders_without_discount = 0
-    orderLength=len(orders)
+    orderLength = len(orders)
+    # Traverse the orders list in the reverse order 
     for order in range(orderLength-1, -1, -1):
         if not orders[order]['coupon_applied']:
             num_orders_without_discount+=1
         else:
             break
-    return num_orders_without_discount+1>=n
-        
-@app.route('/cart_items', methods=['POST'])
-def cart_items(item,code):
-    """Listing all the items in the cart along with the eligibility details"""
-    return jsonify({'items':cart,'isEligible':is_eligible_for_discount()})
+    return num_orders_without_discount+1 >= count
 
-@app.route('/place_order', methods=['POST'])
-def place_order(item,code):
+@app.route('/orders', methods=['GET'])
+def get_orders():
+    """ Get all the previous orders for the user"""
+    global orders
+    return jsonify(orders)
+
+@app.route('/orders', methods=['POST'])
+def place_order():
     """Placing the order"""
-    order={'items':cart,'coupon_applied':False}
+    global orders, cart
+    order=cart
     orders.append(order)
-    return jsonify({order})
+    # Empyt the cart after a successful order
+    cart = {'items' : [], 'total_price' :0, 'discount_eligibility': False, 'coupon_applied' : False, }
+    return jsonify(order)
 
 #Lists count of items purchased, total purchase amount, list of discount codes and total discount amount
 # @app.route('/adminStats',methods=['GET'])
