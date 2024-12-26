97497a54aa098c3a57b219b6349694923c6ac09e
meghavinodkumar <meghasden@gmail.com>
2024-12-26 19:57:01 +0530

    Improved UI and API integration

--- frontend/src/Uniblox.js
+++ frontend/src/Uniblox.js
@@ -1,92 +1,120 @@
-import './Uniblox.css';
+import "./Uniblox.css";
 import unibloxlogo from "./uniblox_io_logo.jpeg";
-import Button from '@mui/material/Button';
-import { TextField } from '@mui/material';
+import Button from "@mui/material/Button";
+import { TextField } from "@mui/material";
 import laptop from "./images/laptop.jpeg";
 import headphone from "./images/headphone.webp";
 import shoes from "./images/shoes.jpg";
 import smartphone from "./images/smartphone.jpg";
 import bag from "./images/bag.webp";
 import watch from "./images/watch.webp";
-import SearchBar from './components/SearchBar';
-import { useState,useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import SearchBar from "./components/SearchBar";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
-const Uniblox=({addToCart})=> {
+const Uniblox = ({ addToCart }) => {
   //hook used for navigation
-  const navigate = useNavigate(); 
-//handle onClick for navigation to the Cart page
+  const navigate = useNavigate();
+  //handle onClick for navigation to the Cart page
   const handleOnClick = () => {
-    navigate('/cart'); 
+    navigate("/cart");
   };
-  const [items] = useState([
-    { name: 'Bag', price: 50, image: bag },
-    { name: 'Laptop', price: 2000, image: laptop },
-    { name: 'Smartphone', price: 1000, image: smartphone },
-    { name: 'Headphone', price: 100, image: headphone },
-    { name: 'Shoes', price: 200, image: shoes },
-    { name: 'Watch', price: 150, image: watch },
-  ]);
-  const [getItem, setGetItem]=useState([]);
-    const addItems = async (item) => {
-      try {
-        const response = await fetch("http://localhost:5000/items", {
-          method: "POST", 
-          headers: {
-            // Set content type to JSON
-            "Content-Type": "application/json", 
-          },
-           // Include the item in the body
-          body: JSON.stringify(item),
-        });
-    
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-    
-        const data = await response.json();
-        console.log("Item added:", data);
-        // Update your state or handle response data as needed
-        setGetItem(data); 
-      } catch (error) {
-        console.error("Failed to add item:", error);
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/products", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((results) => results.json())
+      .then((data) => {
+        console.log(data);
+        setItems(data);
+      });
+  }, []);
+
+  const image_map = [
+    { image_id: 1, image: bag },
+    { image_id: 2, image: laptop },
+    { image_id: 3, image: smartphone },
+    { image_id: 4, image: headphone },
+    { image_id: 5, image: shoes },
+    { image_id: 6, image: watch },
+  ];
+
+  const getImage = (image_id) => {
+    for (var ii = 0; ii < image_map.length; ii++) {
+      if (image_map[ii].image_id == image_id) {
+        return image_map[ii].image;
       }
-    };
-    
+    }
+  };
+
+  const [getItem, setGetItem] = useState([]);
+
+  const addItems = async (item) => {
+    try {
+      const response = await fetch("http://localhost:5000/items", {
+        method: "POST", // Change to POST
+        headers: {
+          "Content-Type": "application/json", // Set content type to JSON
+        },
+        body: JSON.stringify(item), // Include the item in the body
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log("Item added:", data);
+      setGetItem(data); // Update your state or handle response data as needed
+    } catch (error) {
+      console.error("Failed to add item:", error);
+    }
+  };
+
   return (
     <div className="Uniblox">
-      <div className='Uniblox-header'>
+      <div className="Uniblox-header">
         <header className="Uniblox-header-content">
           <img src={unibloxlogo} className="Uniblox-logo" alt="logo" />
           <p className="Uniblox-title">Uniblox</p>
         </header>
         <div className="Uniblox-searchBar">
-        <SearchBar items={items}/>
+          <SearchBar items={items} />
         </div>
         <div className="Uniblox-topMenu">
-          <Button className="Uniblox-topMenu-signin">
-            Sign in
-          </Button>
+          <Button className="Uniblox-topMenu-signin">Sign in</Button>
           <Button className="Uniblox-topMenu-cart" onClick={handleOnClick}>
             Cart
           </Button>
         </div>
       </div>
-          <div className="Uniblox-bodyItems">
-          {items.map((item, index) => (
-            <div className="Uniblox-bodyItems-item" key={index}>
-              <img src={item.image}className="Uniblox-bodyItems-item-img" alt={item.name}/>
-              <p className="Uniblox-bodyItems-item-name">{item.name}</p>
-              <p className="Uniblox-bodyItems-item-price">${item.price}</p>
-              <Button className="Uniblox-bodyItems-item-addItem" onClick={() => addItems(item)}>
-                Add to cart
-              </Button>
-           </div>
-          ))}
-            
-        </div>
+      <div className="Uniblox-bodyItems">
+        {items.map((item, index) => (
+          <div className="Uniblox-bodyItems-item" key={index}>
+            <img
+              src={getImage(item.image_id)}
+              className="Uniblox-bodyItems-item-img"
+              alt={item.name}
+            />
+            <p className="Uniblox-bodyItems-item-name">{item.name}</p>
+            <p className="Uniblox-bodyItems-item-price">${item.price}</p>
+            <Button
+              className="Uniblox-bodyItems-item-addItem"
+              onClick={() => addItems(item)}
+            >
+              Add to cart
+            </Button>
+          </div>
+        ))}
       </div>
+    </div>
   );
-}
+};
 
 export default Uniblox;
--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -1,44 +1,226 @@
-
 import "./Cart.css";
-import * as React from 'react';
-import AspectRatio from '@mui/joy/AspectRatio';
-import Card from '@mui/joy/Card';
-import Typography from '@mui/joy/Typography';
-import Chip from '@mui/material/Chip';
+import * as React from "react";
+import Card from "@mui/joy/Card";
+import Typography from "@mui/joy/Typography";
+import { Button, TextField } from "@mui/material";
+import Stack from "@mui/material/Stack";
+import { useState, useEffect } from "react";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+import laptop from "../images/laptop.jpeg";
+import headphone from "../images/headphone.webp";
+import shoes from "../images/shoes.jpg";
+import smartphone from "../images/smartphone.jpg";
+import bag from "../images/bag.webp";
+import watch from "../images/watch.webp";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+  ...theme.applyStyles("dark", {
+    backgroundColor: "#1A2027",
+  }),
+}));
+
 // This is the Cart page where all the items added will be displayed
-const Cart = () =>{
-    return (
-        <div className="Cart">
-            <div className="Cart-header">
-                Cart Items
-            </div>
-            <Card className="Cart-card"sx={{ height:200,width: 900 }}>
-              <AspectRatio minHeight="100px" maxHeight="200px" >
-                <div className="Cart-card-content">
-                  <Typography level="title-lg">Megha V Kumar</Typography>
-                  <Typography level="body-sm">April 24 to May 02, 2021</Typography>
-                  <Typography level="body-xs">Total price:</Typography>
-                  <Typography sx={{ fontSize: 'lg', fontWeight: 'lg' }}>$2,900</Typography>
-                </div>
-                <img src="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286" srcSet="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286&dpr=2 2x" loading="lazy" alt=""/>
-              </AspectRatio>
-            </Card>
-             {/* ))} */}
-            <div className="Cart-total">
-              <div className="Cart-total-title">
-                Total : $
-              </div>
-              <div className="Cart-total-value">
-                Value
-              </div> 
-            </div>
-            <div className="Cart-coupon">
-              <Chip className="Cart-coupon-text">
-              label="You are eligible to apply coupon" 
-              variant="outlined"
-              </Chip>
-            </div>
+const Cart = () => {
+  const [cart, setCart] = useState([]);
+  const [coupon, setCoupon] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/items", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((results) => results.json())
+      .then((data) => {
+        console.log(data);
+        setCart(data);
+      });
+  }, []);
+  const generateCoupon = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/coupons", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log("Generated Coupon:", data);
+      setCoupon(data.code);
+    } catch (error) {
+      console.error("Failed to add item:", error);
+    }
+  };
+  const apply_coupon = async () => {
+    console.log(coupon);
+    try {
+      const response = await fetch(
+        "http://localhost:5000/apply_coupon/" + coupon,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log("Coupon applied:", data);
+      setCart(data);
+    } catch (error) {
+      console.error("Failed to add item:", error);
+    }
+  };
+  const place_order = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/orders", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setCart({ items: [] });
+      console.log("Coupon applied:", data);
+    } catch (error) {
+      console.error("Failed to add item:", error);
+    }
+  };
+  const image_map = [
+    { image_id: 1, image: bag },
+    { image_id: 2, image: laptop },
+    { image_id: 3, image: smartphone },
+    { image_id: 4, image: headphone },
+    { image_id: 5, image: shoes },
+    { image_id: 6, image: watch },
+  ];
+
+  const getImage = (image_id) => {
+    for (var ii = 0; ii < image_map.length; ii++) {
+      if (image_map[ii].image_id == image_id) {
+        return image_map[ii].image;
+      }
+    }
+  };
+
+  return (
+    <div className="Cart">
+      <div className="Cart-header">Cart Items</div>
+      {cart?.items?.map((item, index) => (
+        <Card className="Cart-card" sx={{ height: 170, width: 600 }}>
+          <img
+            className="Cart-card-image"
+            src={getImage(item.image_id)}
+            loading="lazy"
+            alt=""
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="Cart-card-content"
+            key={index}
+            style={{ marginRight: "15px" }}
+          >
+            <Typography className="Card-item-name" level="title-lg">
+              {item.name}
+            </Typography>
+            <Typography lassName="Card-item-price" level="body-xs">
+              Price : ${item.price}
+            </Typography>
+          </div>
+        </Card>
+      ))}
+      <div className="Cart-footer">
+        <div className="Cart-total">
+          <div className="Cart-total-title">Total : $</div>
+          <div className="Cart-total-value">{cart.total_price}</div>
         </div>
-    );
+
+        <div className="Cart-couponEligibility">
+          <Stack
+            spacing={{ xs: 0.5, sm: 1 }}
+            direction="row"
+            useFlexGap
+            sx={{ flexWrap: "wrap" }}
+          >
+            {cart?.discount_eligibility && (
+              <Button
+                style={{
+                  color: "black",
+                  borderColor: "black",
+                  margin: "10px;",
+                  background: "rgb(197, 191, 191)",
+                }}
+                className="Cart-coupon-text"
+                onClick={() => generateCoupon()}
+                variant="outlined"
+              >
+                <span>You are eligible to apply coupon. Click here</span>
+              </Button>
+            )}
+            {cart?.discount_eligibility && (
+              <TextField
+                sx={{
+                  color: "white",
+                  border: "1px solid white",
+                  borderRadius: "2px",
+                  padding: "1px",
+                  background: "rgb(197, 191, 191)",
+                }}
+                id="outlined-basic"
+                value={coupon}
+              />
+            )}
+            {cart?.discount_eligibility && (
+              <Button
+                style={{
+                  color: "black",
+                  borderColor: "black",
+                  background: "rgb(197, 191, 191)",
+                }}
+                onClick={() => apply_coupon(coupon)}
+              >
+                Apply Coupon
+              </Button>
+            )}
+          </Stack>
+
+          <Button
+            style={{
+              marginTop: "5px",
+              color: "black",
+              borderColor: "black",
+              background: "rgb(197, 191, 191)",
+            }}
+            onClick={() => place_order()}
+          >
+            PLACE ORDER
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Cart;
